067c64d8859adb7587950f9314e3671c63083c74
Tim McEwan <blk_hotsauce@yahoo.com>
2020-04-14 11:32:46 -0600

    Renaming the app and moving to onboarding

--- Pods/Target Support Files/Pods-Lendmeme/Pods-Lendmeme-umbrella.h
+++ Pods/Target Support Files/Pods-Lendmeme/Pods-Lendmeme-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_iborrowVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_iborrowVersionString[];
+FOUNDATION_EXPORT double Pods_LendmemeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LendmemeVersionString[];
 
