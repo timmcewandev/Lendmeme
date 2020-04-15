ecb16f20cc68b65d5f8e410e4e47c847e43d475f
Tim McEwan <blk_hotsauce@yahoo.com>
2020-04-15 16:58:48 -0600

    Adding in number formatter

--- Lendmeme/Controller/BorrowEditorViewController.swift
+++ Lendmeme/Controller/BorrowEditorViewController.swift
@@ -298,6 +298,33 @@ class BorrowEditorViewController: UIViewController, UIImagePickerControllerDeleg
         
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == bottomTextOUT {
+            guard let text = bottomTextOUT.text else { return false }
+            let newString = (text as NSString).replacingCharacters(in: range, with: string)
+            bottomTextOUT.text = formattedNumber(number: newString)
+            return false
+        }
+        return true
+    }
+    
+    func formattedNumber(number: String) -> String {
+        let cleanPhoneNumber = number.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        let mask = "(XXX) XXX-XXXX"
+
+        var result = ""
+        var index = cleanPhoneNumber.startIndex
+        for ch in mask where index < cleanPhoneNumber.endIndex {
+            if ch == "X" {
+                result.append(cleanPhoneNumber[index])
+                index = cleanPhoneNumber.index(after: index)
+            } else {
+                result.append(ch)
+            }
+        }
+        return result
+    }
+    
     @objc func doneAction() {
         bottomTextOUT.resignFirstResponder()
     }
